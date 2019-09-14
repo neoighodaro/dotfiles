faeb50c675c00b692cac06d5705313ba6aaab793
Neo Ighodaro <neo@creativitykills.co>
2019-09-14 09:46:01 +0200

    small updates

--- init.sh
+++ init.sh
@@ -1,15 +1,4 @@
-if [[ -f "$HOME/.dotfiles/.zshrc_custom" ]]; then
-    source $HOME/.dotfiles/.zshrc_custom
-fi
-
-if [[ -f "$HOME/.dotfiles/private.sh" ]]; then
-    source $HOME/.dotfiles/private.sh
-fi
-
-if [[ -f "$HOME/.dotfiles/aliases.sh" ]]; then
-    source $HOME/.dotfiles/aliases.sh
-fi
-
-if [[ -f "$HOME/.dotfiles/functions.sh" ]]; then
-    source $HOME/.dotfiles/functions.sh
-fi
+[[ -f "$HOME/.dotfiles/.zshrc_custom" ]] && source $HOME/.dotfiles/.zshrc_custom
+[[ -f "$HOME/.dotfiles/private.sh" ]] && source $HOME/.dotfiles/private.sh
+[[ -f "$HOME/.dotfiles/aliases.sh" ]] && source $HOME/.dotfiles/aliases.sh
+[[ -f "$HOME/.dotfiles/functions.sh" ]] && source $HOME/.dotfiles/functions.sh

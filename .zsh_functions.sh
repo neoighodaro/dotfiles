eb78696ba694327621f4006daf3239794b48b3de
Neo Ighodaro <neo@creativitykills.co>
2021-02-20 09:54:04 +0100

    Updated the ios function to remove unnecessary function

--- .zsh_functions.sh
+++ .zsh_functions.sh
@@ -114,22 +114,6 @@ bak() {
     cp -R "${1}" "${SAVEBAKPATH}"
 }
 
-# Usage: wordpress_new [name]
-wordrpess_new() {
-	mkdir "$1"
-	cd "$1"
-	wget "http://wordpress.org/latest.zip"
-	unzip latest.zip
-	rm -rf __MACOSX latest.zip
-	cp -rf ./wordpress/* ./
-	rm -rf ./wordpress/ ./wp-content/plugins/hello.php ./readme.html
-	mkdir ./wp-content/uploads/
-	mv wp-config-sample.php wp-config.php
-	touch htaccess.txt robots.txt
-	nano wp-config.php
-	open https://api.wordpress.org/secret-key/1.1/salt/
-}
-
 
 # ----------------------------------------------------------------------------------------
 # iOS

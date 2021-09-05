620a23daa1ad08cb692b876be4161692c4d7cf66
Neo <neo@creativitykills.co>
2021-09-05 16:35:10 +0200

    Update install.sh

--- .vim/install.sh
+++ .vim/install.sh
@@ -5,7 +5,7 @@ CURR_DIR=$(cd `dirname $0` && pwd)
 if [[ ! -f "${CURR_DIR}/colors/atom-dark-256.vim" ]]; then
     git clone https://github.com/gosukiwi/vim-atom-dark.git "${CURR_DIR}/colors/temp_theme"
     mv "${CURR_DIR}/colors/temp_theme/colors/atom-dark-256.vim" "${CURR_DIR}/colors/atom-dark-256.vim"
-    rm -R "${CURR_DIR}/colors/temp_theme"
+    sudo rm -R "${CURR_DIR}/colors/temp_theme"
 fi
 
 # Install VundleVim

b548deea18a203e403cd4fb3d7c7f98ee05f4ffc
Neo Ighodaro <public@neoi.sh>
2024-11-26 08:40:50 +0100

    Fixed lazygit installaton

--- misc/init-linux-install-lazygit.sh
+++ misc/init-linux-install-lazygit.sh
@@ -3,11 +3,32 @@
 if [[ -f "/usr/local/bin/lazygit" ]]; then
     echo -e "${GRAY}===> Lazygit is already installed. Skipping...${NC}"
 else
-    # get latest version
-    LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | \grep -Po '"tag_name": *"v\K[^"]*')
+    # Get the latest version
+    LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": *"v\K[^"]*')
 
-    # download...
-    curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/download/v${LAZYGIT_VERSION}/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
+    # Determine the architecture
+    ARCH=$(uname -m)
+    OS=$(uname -s | tr '[:upper:]' '[:lower:]')
+
+    if [[ $ARCH == "x86_64" ]]; then
+        ARCH="x86_64"
+    elif [[ $ARCH == "arm64" ]] || [[ $ARCH == "aarch64" ]]; then
+        ARCH="arm64"
+    elif [[ $ARCH == "armv6" ]]; then
+        ARCH="armv6"
+    elif [[ $ARCH == "i386" ]] || [[ $ARCH == "i686" ]]; then
+        ARCH="32-bit"
+    else
+        echo -e "${RED}Unsupported architecture: ${ARCH}${NC}"
+        exit 1
+    fi
+
+    # Construct download URL
+    FILENAME="lazygit_${LAZYGIT_VERSION}_${OS}_${ARCH}.tar.gz"
+    DOWNLOAD_URL="https://github.com/jesseduffield/lazygit/releases/download/v${LAZYGIT_VERSION}/${FILENAME}"
+
+    # Download the Lazygit binary
+    curl -Lo lazygit.tar.gz "${DOWNLOAD_URL}"
 
     # Unzip
     tar xf lazygit.tar.gz lazygit

43b66b005a97db636cda5100fe901134ac082158
Neo Ighodaro <neo.ighodaro@pinktum.com>
2025-05-18 14:12:02 +0200

    Small update

--- sketchybar/colors.sh
+++ sketchybar/colors.sh
@@ -3,22 +3,37 @@
 
 export WHITE=0xffffffff
 
+# Ekko Scheme
+# -------------------------------------------------------------------------------------------
+# 0xff1b140b
+export BAR_BG_COLOR=0x40000000
+export ITEM_BG_COLOR=0xff9a4423
+export TEXT_COLOR=0xffffffff
+export TEXT_COLOR_DIM=0x5f0fffff
+export ACTIVE_APP_TEXT_COLOR=0xff9a4423
+export ACTIVE_APP_BG_COLOR=0xfffad88b
+export AEROSPACE_TEXT_COLOR=0xffbdbec0
+export AEROSPACE_BG_COLOR=0x40000000
+export AEROSPACE_ACTIVE_TEXT_COLOR=0xff9a4423
+export AEROSPACE_ACTIVE_BG_COLOR=0xfffad88b
+
 # Teal Scheme
 # -------------------------------------------------------------------------------------------
 # export BAR_BG_COLOR=0xff001f30
-export ITEM_BG_COLOR=0xff003547
-export TEXT_COLOR=0xffffffff
-export TEXT_COLOR_DIM=0x5f0fffff
-export ACTIVE_APP_TEXT_COLOR=0xff003547
-export ACTIVE_APP_BG_COLOR=0xff2cf9ed
-export AEROSPACE_TEXT_COLOR=0xff003547
-export AEROSPACE_ACTIVE_TEXT_COLOR=0xff2cf9ed
-export AEROSPACE_BG_COLOR=0x40000000
-export AEROSPACE_ACTIVE_BG_COLOR=0xff003547
+# export BAR_BG_COLOR=0x40000000
+# export ITEM_BG_COLOR=0xff003547
+# export TEXT_COLOR=0xffffffff
+# export TEXT_COLOR_DIM=0x5f0fffff
+# export ACTIVE_APP_TEXT_COLOR=0xff003547
+# export ACTIVE_APP_BG_COLOR=0xff2cf9ed
+# export AEROSPACE_TEXT_COLOR=0xff003547
+# export AEROSPACE_ACTIVE_TEXT_COLOR=0xff2cf9ed
+# export AEROSPACE_BG_COLOR=0x40000000
+# export AEROSPACE_ACTIVE_BG_COLOR=0xff003547
 
 # CK Yellow Scheme
 # -------------------------------------------------------------------------------------------
-export BAR_BG_COLOR=0x40000000
+# export BAR_BG_COLOR=0x40000000
 # export ITEM_BG_COLOR=0xff2d2b27
 # export TEXT_COLOR=0xffffd676
 # export TEXT_COLOR_DIM=0x992d2b27
